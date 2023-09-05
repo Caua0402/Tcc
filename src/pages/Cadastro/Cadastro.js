b1e89084696fea2a3e1956c245f2b13f73722d07
VINICIUS NUNES DE OLIVEIRA <22282114@dsn127m1246726.cfp127aluno.local>
2023-09-05 08:16:13 -0300

    first commit

---
+++ src/pages/Cadastro/Cadastro.js
@@ -0,0 +1,7 @@
+export const Cadastro = () => {
+  return (
+  <>
+    <h1>Aqui é o Cadastro</h1>
+  </>
+  );
+}
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,9 @@
+import logo from '../../assets/logo.svg'
+export const Home = () => {
+  return (
+  <>
+    <h1>Aqui é a home</h1>
+    <img src={logo}/>
+  </>
+  );
+}
